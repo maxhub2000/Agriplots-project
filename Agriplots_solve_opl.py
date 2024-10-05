3f5c68e2271e33ba557381bbf254e7639a5eac5c
maxhub2000 <62055643+maxhub2000@users.noreply.github.com>
2024-10-06 00:06:17 +0300

    Add files via upload
    Fixed the logic regarding energy consumption by yeshuvim.
    Now it includes correctly (in the correct order) on yeshuvim that appear in the dataset, and for location that are in yeshuvim which are not in the dataset, we'll assign energy consumption of 0,
    so that the model won't choose to install PV in those locations.
    In the future we'll decide what to do with those locations.

--- Agriplots_solve_opl.py
+++ Agriplots_solve_opl.py
@@ -2,49 +2,100 @@ import subprocess
 import shutil
 import pandas as pd
 
-def create_yeshuvim_with_locations(df, yeshuvim_in_eshkolot):
-    yeshuvim_in_df = df['YeshuvName']
+def create_yeshuvim_with_locations(df_, yeshuvim_in_eshkolot):
+    yeshuvim_in_df = df_['YeshuvName']
     yeshuvim_eshkolot_in_df = pd.merge(yeshuvim_in_df, yeshuvim_in_eshkolot, on='YeshuvName', how='inner')
     yeshuvim_with_locations = {}
     eshkolot_with_locations = {}
     location_to_object_id = {}
     for i in yeshuvim_eshkolot_in_df.index:
         location = i+1
-        location_to_object_id[df["OBJECTID"][i]] = location
+        location_to_object_id[df_["OBJECTID"][i]] = location
 
-        yeshuv = yeshuvim_eshkolot_in_df['YeshuvName'][i]
-        if yeshuv not in yeshuvim_with_locations:
-            yeshuvim_with_locations[yeshuv] = {location}
-        else:
-            yeshuvim_with_locations[yeshuv].add(location)
 
         eshkol = yeshuvim_eshkolot_in_df['eshkol'][i]
         if eshkol not in eshkolot_with_locations:
             eshkolot_with_locations[eshkol] = {location}
         else:
             eshkolot_with_locations[eshkol].add(location)
-    return yeshuvim_with_locations, eshkolot_with_locations, location_to_object_id
 
+    # print("yeshuvim_with_locations before making it simple:\n ", yeshuvim_with_locations)
+    # print("len(yeshuvim_with_locations): ", len(yeshuvim_with_locations))
 
-def prepare_data(df, energy_consumption_by_yeshuv, influence_on_crops_dict, yeshuvim_with_locations, eshkolot_with_locations, energy_division_between_eshkolot):
+    yeshuvim_with_locations = df_[['location_id', 'YeshuvName']]
+    #eshkolot_with_locations = []
+    location_to_object_id = []
 
-    
-    fix_energy_production = df['Energy production (fix) mln kWh/year'].tolist()
-    total_revenue = df['Total revenue, mln NIS'].tolist()
-    area_in_dunam = df['Dunam'].tolist()
-    # maps average influence on crops to each AnafSub according to the influence_on_crops_dict (like Vlookp)
-    influence_on_crops = df['AnafSub'].map(influence_on_crops_dict).tolist()
+    return eshkolot_with_locations, location_to_object_id
+
+def create_locations_in_yeshuvim(yeshuvim_with_locations):
+    D = {}
+    for YeshuvName in yeshuvim_with_locations["YeshuvName"].unique():
+        # Get all location IDs associated with the current YeshuvName
+        location_ids = yeshuvim_with_locations.loc[yeshuvim_with_locations['YeshuvName'] == YeshuvName]["location_id"].tolist()
+        if YeshuvName not in D:
+            D[YeshuvName] = set(location_ids)  # Create a set with all locations for the YeshuvName
+        else:
+            D[YeshuvName].update(location_ids)  # Update the set with additional location_ids if any   
+    return D
+  
+def adjust_energy_consumption_by_yeshuv(energy_consumption_by_yeshuv_, relevant_yeshuvim_):
+    energy_consumption_by_yeshuv_ = energy_consumption_by_yeshuv_.drop(['yeshuv_symbol'], axis=1) # remove yeshuv_symbol column
+    # creating energy consumption by yeshuv for only cities in inputed df (relevant_yeshuvim_),
+    # if city not in original energy_consumption_by_yeshuv, their yearly consumption will be 0
+    adjusted_df = pd.DataFrame(columns=['yeshuv_name', 'yearly energy consumption'])
+    for yeshuv in relevant_yeshuvim_:
+        if yeshuv in energy_consumption_by_yeshuv_["yeshuv_name"].tolist():
+            # if yeshuv in input df, get it's yeshuv name and yearly energy consumption in the "row" var, then append the row to the adjusted_df
+            row = energy_consumption_by_yeshuv_.loc[energy_consumption_by_yeshuv_['yeshuv_name'] == yeshuv]
+            adjusted_df = adjusted_df.append(row, ignore_index = True)
+        else:
+            # otherwise, add the yeshuv name with yearly energy consumption of 0, so that the model wouldn't choose locations from that yeshuv
+            row = {'yeshuv_name': yeshuv, 'yearly energy consumption': 0.00}
+            adjusted_df = adjusted_df.append(row, ignore_index = True)
+    return adjusted_df
+
+def sort_df_by_list_order(df_, list_order, column_name):
+    """
+    Sort a DataFrame by the order of values in a list.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame to sort.
+    list_order (list): A list of values specifying the desired order.
+    column_name (str): The column in df to be sorted based on the list_order.
+
+    Returns:
+    pd.DataFrame: A new DataFrame sorted according to the list_order.
+    """
+    # Convert the specified column to a categorical type with the provided order
+    df_[column_name] = pd.Categorical(df_[column_name], categories=list_order, ordered=True)
+    # Sort the DataFrame by the specified column
+    df_sorted = df_.sort_values(column_name).reset_index(drop=True)
+    return df_sorted
+
+def prepare_data(df_, energy_consumption_by_yeshuv, eshkolot_with_locations, energy_division_between_eshkolot):
+    fix_energy_production = df_['Energy production (fix) mln kWh/year'].tolist()
+    total_revenue = df_['Total revenue, mln NIS'].tolist()
+    area_in_dunam = df_['Dunam'].tolist()
+    influence_on_crops = df_['Average influence of PV on crops'].tolist()
     num_locations = len(fix_energy_production)
-    
-    num_yeshuvim = len(yeshuvim_with_locations)
-    relevant_yeshuvim = yeshuvim_with_locations.keys()
-    # takes only yeshuvim that appear in the current dataset
-    energy_consumption_by_yeshuv = [row['yearly energy consumption'] for index, row in energy_consumption_by_yeshuv.iterrows() if row['yeshuv_name'] in relevant_yeshuvim]
-    
+
+    yeshuvim_with_locations = df_[['location_id', 'YeshuvName']]
+    locations_in_yeshuvim = create_locations_in_yeshuvim(yeshuvim_with_locations)
+    yeshuvim_with_locations = locations_in_yeshuvim
+    # takes only yeshuvim that appear in the current dataset; values are unique since yeshuvim_with_locations is a dictionary
+    relevant_yeshuvim = list(yeshuvim_with_locations.keys())
+    num_yeshuvim = len(relevant_yeshuvim)
+    # adjust energy consumptions so that it will only include yeshuvim from the dataset, and then convert it to a list
+    energy_consumption_by_yeshuv = adjust_energy_consumption_by_yeshuv(energy_consumption_by_yeshuv, relevant_yeshuvim)
+    energy_consumption_by_yeshuv = energy_consumption_by_yeshuv['yearly energy consumption'].tolist()
+
+
     num_eshkolot = len(eshkolot_with_locations)
     relevant_eshkolot = eshkolot_with_locations.keys()
     # takes only eshkolot that appear in the current dataset
     energy_division_between_eshkolot = [row['percentage_of_energy_output'] for index, row in energy_division_between_eshkolot.iterrows() if row['eshkol'] in relevant_eshkolot]
+
 
     return {
         "num_locations" : num_locations, #need to be first here, otherwise there will be bug in the mod file
@@ -112,20 +163,59 @@ def solve_opl_model(mod_file, dat_file, output_file=None):
         print(f"Error running oplrun: {e}")
 
 
-def main():
-    # File paths and parameters
-    opl_model_file = 'Agriplots.mod'
-    dat_file = 'Agriplots.dat'
-    df_dataset = pd.read_excel('Agriplots dataset - 1000 rows.xlsx')
-    output_file = 'output.txt'
-    energy_consumption_by_yeshuv = pd.read_excel("energy_consumption_by_yeshuv-average_consumption_times_population_per_yeshuv.xlsx")
-    yeshuvim_in_eshkolot = pd.read_excel('yeshuvim_in_eshkolot.xlsx')
-    yeshuvim_in_eshkolot.rename(columns = {'eshkol_2021':'eshkol'}, inplace = True)
-    energy_division_between_eshkolot = pd.read_excel('energy_division_between_eshkolot-synthetic_values.xlsx')
+
+def modify_influence_on_crops(df_, synthetic_values_of_influence_on_crops_path):
     # prepare dictionary that maps for each crop how much it's influenced by installing PV
-    influence_on_crops_data = pd.read_excel('Average influence of PV on crops - synthetic values.xlsx')
+    influence_on_crops_data = pd.read_excel(synthetic_values_of_influence_on_crops_path)
     influence_on_crops_data["Average influence"] = (influence_on_crops_data["Average influence"]-1).round(2)
     influence_on_crops_dict = influence_on_crops_data.set_index("AnafSub")["Average influence"].to_dict()
+    # maps average influence on crops to each AnafSub according to the influence_on_crops_dict (like Vlookp)
+    modified_influence_on_crops = df_['AnafSub'].map(influence_on_crops_dict).tolist()
+    df_["Average influence of PV on crops"] = modified_influence_on_crops
+    return df_
+
+def remove_rows_with_missing_values(df_):
+    # remove rows with nan values (Ideally should find a better way to handle those nan values later on)
+    df_ = df_.dropna(subset=['Energy production (fix) mln kWh/year',
+                           'Average influence of PV on crops',
+                           'Total revenue, mln NIS',
+                           'Dunam'])
+    return df_
+
+def add_eshkolot_to_dataset(df_, yeshuvim_in_eshkolot_):
+    # Perform a left join to add the 'eshkol' column from yeshuvim_in_eshkolot_ to df_
+    df_ = pd.merge(df_, yeshuvim_in_eshkolot_, on='YeshuvName', how='left')
+    # Fill missing values (if a YeshuvName is not found in yeshuvim_in_eshkolot_, set 'eshkol' to -1)
+    df_['eshkol'].fillna(-1, inplace=True)
+    # Optionally, convert 'eshkol' to integer if needed
+    df_['eshkol'] = df_['eshkol'].astype(int)
+    return df_
+
+
+
+
+def main():
+    # File paths and parameters
+    opl_model_file, dat_file, output_file = 'Agriplots.mod', 'Agriplots.dat', 'output.txt'
+    df_dataset = pd.read_excel('Agriplots dataset - 1000 rows.xlsx') # Read dataset from Excel
+    df_dataset["location_id"] = df_dataset.index + 1
+    print("df_dataset[location_id]\n", df_dataset["location_id"])
+    df_dataset = remove_rows_with_missing_values(df_dataset)
+    # modify influence on crops column based on synthetic values
+    df_dataset = modify_influence_on_crops(df_dataset, 'Average influence of PV on crops - synthetic values.xlsx')
+
+    energy_consumption_by_yeshuv = pd.read_excel("energy_consumption_by_yeshuv-average_consumption_times_population_per_yeshuv.xlsx")
+    
+
+    #yeshuvim_in_eshkolot = pd.read_excel('yeshuvim_in_eshkolot.xlsx')
+    yeshuvim_in_eshkolot = pd.read_excel('yeshuvim_in_eshkolot_modified_to_match_dataset.xlsx')
+    yeshuvim_in_eshkolot.rename(columns = {'eshkol_2021':'eshkol'}, inplace = True)
+    energy_division_between_eshkolot = pd.read_excel('energy_division_between_eshkolot-synthetic_values.xlsx')
+    # add eshkolot to dataset based on yeshuvim_in_eshkolot
+    df_dataset = add_eshkolot_to_dataset(df_dataset, yeshuvim_in_eshkolot)
+    print("df_dataset after adding eshkolot:\n", df_dataset)
+    
+    
     # parameters of the model
     params = {
         "influence_on_crops_lower_limit": 0.00,
@@ -133,14 +223,10 @@ def main():
         "total_area_upper_bound": 1500.00
     }
 
-    # remove rows with nan values (Ideally should find a better way to handle those nan values later on)
-    df_dataset = df_dataset.dropna(subset=['Energy production (fix) mln kWh/year',
-                           'Average influence of PV on crops',
-                           'Total revenue, mln NIS',
-                           'Dunam'])
 
 
-    yeshuvim_with_locations, eshkolot_with_locations, location_to_object_id = create_yeshuvim_with_locations(df_dataset, yeshuvim_in_eshkolot)
+
+    eshkolot_with_locations, location_to_object_id = create_yeshuvim_with_locations(df_dataset, yeshuvim_in_eshkolot)
     ###plan regarding using the location_to_object_id dict:
     # If I have that dictionary, I could have a "connecting table" between the results of the opl model,
     # which is in term of location (a number) and the original data of each field, which is in term of OBJECTID.
@@ -150,8 +236,8 @@ def main():
     # in the model (sanity check) etc.
     # I should probably implement that in a seperate python file that will import stuff from this file, and output the resluts as xlsx file.
 
-    # Read data from Excel
-    data = prepare_data(df_dataset, energy_consumption_by_yeshuv, influence_on_crops_dict, yeshuvim_with_locations, eshkolot_with_locations, energy_division_between_eshkolot)
+    
+    data = prepare_data(df_dataset, energy_consumption_by_yeshuv, eshkolot_with_locations, energy_division_between_eshkolot)
     # Write data to .dat file
     write_dat_file(dat_file, data, params)
     # Solve the OPL model
